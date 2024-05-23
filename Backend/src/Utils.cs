5362dbbb737a02101844ec1cf7a617d5eea562c1
MelihOcal <145652890+MelihOcal@users.noreply.github.com>
2024-05-23 12:28:00 +0200

    all test is working

--- Backend/src/Utils.cs
+++ Backend/src/Utils.cs
@@ -61,9 +61,51 @@ public static class Utils
                 successFullyWrittenUsers.Push(user);
             }
         }
+
         return successFullyWrittenUsers;
+
+
     }
 
-    // Now write the two last ones yourself!
-    // See: https://sys23m-jensen.lms.nodehill.se/uploads/videos/2021-05-18T15-38-54/sysa-23-presentation-2024-05-02-updated.html#8
-}
+   /* public static Arr RemoveMockUsers()
+    {
+    // Lista för att hålla de användare som faktiskt togs bort
+    Arr successfullyRemovedUsers = new Arr();
+
+    foreach (var user in mockUsers)
+    {
+        // Kontrollera om användaren finns i databasen
+        var result = SQLQueryOne(
+            "SELECT * FROM users WHERE email = $email",
+            new { email = user.email }
+        );
+
+        // Om användaren finns i databasen, ta bort användaren
+        if (result != null && !result.HasKey("error"))
+        {
+            // Ta bort användaren
+            var deleteResult = SQLQueryOne(
+                "DELETE FROM users WHERE email = $email RETURNING firstName, lastName, email",
+                new { email = user.email }
+            );
+
+            // Om borttagningen lyckades, lägg till användaren i listan
+             if (!result.HasKey("error"))
+            {
+                successfullyRemovedUsers.Push(deleteResult);
+            }
+        }
+    }
+   return successfullyRemovedUsers;
+
+    }*/
+
+
+    public static Obj EmailDomainCounter(){
+        var resultFromDb = SQLQuery("SELECT * FROM emailDomainCounter");
+        var result = Obj();
+        resultFromDb.ForEach(row => result[row.domain] = row.counter );
+        return result;
+    }
+
+ }
